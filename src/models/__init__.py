ff5a2649bb22868550c46bbd6695ccb705de4aef
Ilyas Fardaoui <ilyasfardaoui@gmail.com>
2026-03-13 14:15:27 +0000

    AI: improve Python file src\models\__init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,11 @@
 from .gradient_boosting_pipeline import GradientBoostingPipeline
 from .imbalanced_handler import ImbalancedDataHandler
 
-__all__ = ['GradientBoostingPipeline', 'ImbalancedDataHandler']
+"""
+Module containing machine learning pipelines and handlers.
 
+This module provides two main classes: GradientBoostingPipeline and ImbalancedDataHandler.
+These classes are designed to be used in conjunction with each other to create and train machine learning models.
+"""
+
+__all__ = ['GradientBoostingPipeline', 'ImbalancedDataHandler']
